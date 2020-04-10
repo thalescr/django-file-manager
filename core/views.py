a6888954d0a6f30c865e0ed0f901687284d3f8d6
Thales Rosa <thalescr@protonmail.com>
2020-04-10 14:26:39 -0300

    Added delete and upload buttons

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView, View
 from django.shortcuts import redirect
 from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
 import os
 from project.settings import MEDIA_ROOT
 
@@ -18,13 +19,13 @@ class Directory(TemplateView):
             # Check if requested path is within MEDIA_ROOT
             if os.path.commonprefix([dir, MEDIA_ROOT]) == MEDIA_ROOT:
                 if os.path.exists(dir):
-                    return os.scandir(dir)
+                    return dir
         return None
 
     # Send directory info and requested path to template
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['dir'] = self.get_directory()
+        context['dir'] = os.scandir(self.get_directory())
         context['path'] = self.request.GET.get('dir')
         return context
 
@@ -35,6 +36,17 @@ class Directory(TemplateView):
         # Otherwise redirect to MEDIA_ROOT directory
         else:
             return redirect('/?dir=.')
+
+    # Save uploaded file into current folder
+    def post(self, *args, **kwargs):
+        dir = self.get_directory()
+        uploaded_file = self.request.FILES['document']
+        if dir:
+            fs = FileSystemStorage(location=dir)
+            fs.save(uploaded_file.name, uploaded_file)
+        return self.render_to_response(self.get_context_data())
+        
+
 
 # Download view
 class Download(View):
